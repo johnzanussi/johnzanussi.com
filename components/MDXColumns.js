c0417a27a2cbbc77e85b8e3a5ef98d897ec9bf4b
John Zanussi <johnzanussi@gmail.com>
2022-06-23 10:03:39 -0400

    Ignore invalid elements in mdxcolumns

--- components/MDXColumns.js
+++ components/MDXColumns.js
@@ -1,13 +1,19 @@
-import { Children } from 'react';
+import { Children, isValidElement } from 'react';
 
 const MDXColumns = ({ children }) => {
-    const colSize = Math.max(12 / Children.count(children), 3);
+    const childCount = Children.toArray(children).filter((child) =>
+        isValidElement(child)
+    ).length;
+    const colSize = Math.max(12 / childCount, 3);
 
     return (
         <div className="row">
-            {Children.map(children, (child) => (
-                <div className={`col-md-${colSize}`}>{child}</div>
-            ))}
+            {Children.map(children, (child) => {
+                if (!isValidElement(child)) {
+                    return null;
+                }
+                return <div className={`col-md-${colSize}`}>{child}</div>;
+            })}
         </div>
     );
 };
