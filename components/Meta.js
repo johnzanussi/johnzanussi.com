a1021bd0a56d11dd3cb5c9f1194fc52c083f9140
John Zanussi <johnzanussi@gmail.com>
2022-08-26 13:43:42 -0400

    prepend host url to og image tags

--- components/Meta.js
+++ components/Meta.js
@@ -1,15 +1,25 @@
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
+import { prependHost } from '../lib/utils';
 
 const Meta = ({ title, description, openGraph, ...props }) => {
     const { asPath } = useRouter();
 
-    const url = `${process.env.NEXT_PUBLIC_SITE_URL}${asPath}`;
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
+
+    const url = prependHost(asPath);
 
     if (props.images) {
         openGraph.images = props.images;
     } else if (props.image) {
         openGraph.images = [props.image];
+    }
+
+    if (openGraph.images) {
+        openGraph.images = openGraph.images.map((image) => ({
+            ...image,
+            url: prependHost(image.url),
+        }));
     }
 
     return (
