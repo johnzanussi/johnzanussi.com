7de799de6678a650cc68975cc145ad4ba427a2ab
John Zanussi <johnzanussi@gmail.com>
2023-02-14 12:12:15 -0500

    Remove separate onClick util since it didn't decrease JS asset size

--- src/scripts/onClick.js
+++
@@ -1,4 +0,0 @@
-export default function onClick (selector, clickHandler) {
-    const elements = document.querySelectorAll(selector);
-    elements.forEach((element) => element.addEventListener('click', clickHandler));
-}
